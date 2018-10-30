8f2b5b420cac3318a1694d5a08fb1726b79d8026
= <383961310@qq.com>
2018-10-30 23:24:02 +0800

    EchoServerHandler handler

---
+++ netty/src/main/java/com/kidding/netty/EchoServer.java
@@ -0,0 +1,10 @@
+package com.kidding.netty;
+
+/**
+ * Created by Administrator on 2018/10/30.
+ */
+public class EchoServer {
+
+
+
+}
---
+++ netty/src/main/java/com/kidding/netty/handler/EchoServerHandler.java
@@ -0,0 +1,39 @@
+package com.kidding.netty.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * Created by Administrator on 2018/10/30.
+ * <p>
+ * ChannelInboundHandlerAdapter默认实现了ChannelInboundHandler
+ * 我们只需要重写我们感兴趣的方法即可
+ */
+@ChannelHandler.Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("received: " + in.toString(CharsetUtil.UTF_8));
+        ctx.write(msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)
+                .addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
