3a27e302ca0939ceb885a8e5ab17cdcc8f907951
= <383961310@qq.com>
2018-11-01 22:34:48 +0800

    EchoServer

--- netty/src/main/java/com/kidding/netty/EchoServer.java
+++ netty/src/main/java/com/kidding/netty/EchoServer.java
@@ -1,10 +1,54 @@
 package com.kidding.netty;
 
+import com.kidding.netty.handler.EchoServerHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
 /**
  * Created by Administrator on 2018/10/30.
+ * server 启动类
  */
 public class EchoServer {
 
+    private int port;
 
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws Exception {
+        new EchoServer(8080).start();
+    }
+
+    public void start() throws Exception {
+
+        final EchoServerHandler serverHandler = new EchoServerHandler();
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(group)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(serverHandler);
+                        }
+                    });
+
+            ChannelFuture channelFuture = serverBootstrap.bind().sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
 
 }
